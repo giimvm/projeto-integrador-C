712c25cf3764725061b1ac8ccbdea782d8bab998
lannadev07 <lannamarculino15@gmail.com>
2026-09-11 18:11:48 -0300

    testecommit

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(void) {
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("CONSEGUI CARALHO CHUPAAAAA RAYLIB", 200, 200, 20, RED);
+        DrawText("APRENDI RAYLIB ESPERO", 201, 200, 24, PINK);
         EndDrawing();
     }
 
